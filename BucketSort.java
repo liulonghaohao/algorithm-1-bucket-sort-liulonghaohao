9edaa324e80336ed88c6fb0c2f635adad3bfc4e4
liulonghaohao <45375735+liulonghaohao@users.noreply.github.com>
2018-12-17 19:21:29 +0800

    桶排序提交

--- BucketSort.java
+++ BucketSort.java
@@ -4,18 +4,21 @@ int [] a = {2,1,2,100};
 3.对应新的数组下标，然后与原数组数相同，将此下标的数组内+1；
 4.返回新的数组。
 
-      int temp=a[0];
-    for(int j=0;j<a.length;j++){
-    if(a[i]>temp){
-    temp=a[i];
-}
-    
-  int []b = new int [temp+1];
-  for(int i=0; i<b.length;i++){
-  if(i==a[j]){
-    b[i]+=1
-  }
+		int temp=a[0];
+	      for(int i=1;i<a.length;i++){
+	      if(a[i]>temp){
+	      temp=a[i];
+	       }
+	      }
+	      
+	    int []b = new int [temp+1];
+	    for(int k=0; k<b.length;k++){
+	          for(int j=0;j<a.length;j++){
+	                if(k==a[j]){
+	                    System.out.print(k);
+	                 }
+               }
+	         }
+ 
 
-}
-      System.out.print(b);
- }
+
